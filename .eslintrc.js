5fc894605928e61d0d252fbf4dc3ebc69a4a27b2
Thomas Weustenfeld <tw@econify.com>
2021-12-13 16:00:42 +0100

    :tada: init checkin

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    // '@typescript-eslint/explicit-member-accessibility': 0,
+    // '@typescript-eslint/explicit-function-return-type': 0,
+    // '@typescript-eslint/no-parameter-properties': 0,
+    // '@typescript-eslint/interface-name-prefix': 0,
+  },
+};
---
+++ README.md
@@ -0,0 +1,17 @@
+# graphql-demo
+
+GraphQL Demos.
+
+## Run locally
+
+```bash
+npm install
+```
+
+```bash
+npm run dev
+# or
+npm run start
+# or
+npm run inspect
+```
